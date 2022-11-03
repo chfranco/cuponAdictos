bb63143db16c3b15c4126f9ca0f6c3976f283aa3
frcher <cristhian.h.franco@gmal.com>
2022-11-03 02:21:46 -0500

    Add functional menu

--- appCupones/js/main.js
+++ appCupones/js/main.js
@@ -43,3 +43,26 @@ function mostrarModal(){
     }
 }
 
+//menu
+window.addEventListener("load", () => {
+  document.body.classList.remove("preload")
+})
+
+document.addEventListener('DOMContentLoaded', () =>{
+  const nav = document.querySelector(".nav")
+
+  document.querySelector("#btnNav").addEventListener("click", ()=>{
+      nav.classList.add("nav-open")
+  })
+
+  document.querySelector(".nav-overlay").addEventListener("click", ()=>{
+      nav.classList.remove("nav-open")
+  })
+
+
+  document.querySelector(".close").addEventListener("click", ()=>{
+      nav.classList.remove("nav-open")
+  })
+
+})
+
